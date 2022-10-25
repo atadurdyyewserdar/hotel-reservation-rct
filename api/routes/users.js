b0c878e7c339878386a32623605db42548a44dcb
atadurdyyewserdar <atadurdyyewserdar@gmail.com>
2022-10-25 03:47:07 +0300

    Completed user jwt authentication and crud operations on hotels, rooms and hotels in minimal

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,15 +1,36 @@
 import express from "express";
-import {getUsers, deleteUser, getUser, updateUser} from "../controllers/users.js";
+import {
+  getUsers,
+  deleteUser,
+  getUser,
+  updateUser,
+} from "../controllers/users.js";
+import { verifyAdmin, verifyToken, verifyUser } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
+// router.get("/checkauthentication", verifyToken, (req, res, next) => {
+//   res.send("Hello user, u are authenticated");
+// });
+
+// router.get("/checkuser/:id", verifyUser, (req, res, next) => {
+//   res.send("Hello user, u are authenticated and u can delete your account");
+// });
+
+// router.get("/checkadmin/:id", verifyAdmin, (req, res, next) => {
+//   res.send("Hello user, u are authenticated and u can delete all accounts");
+// });
+
+
 //UPDATE
-router.put("/:id", updateUser);
+router.put("/:id", verifyUser, updateUser);
 //DELETE
-router.delete("/:id", deleteUser);
+router.delete("/:id", verifyUser, deleteUser);
 //GET
-router.get("/:id", getUser);
+router.get("/:id", verifyUser, getUser);
 //GET ALL
-router.get("/", getUsers);
+router.get("/", verifyAdmin, getUsers);
 
 export default router;
+
+////634f4c3596957e9d03703623
